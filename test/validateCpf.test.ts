3896e5d3bfd6b32164af9eb389a25f1da3c02a1b
yThiagoFS <thi.ferreira.silva03@gmail.com>
2024-04-16 21:46:09 -0300

    first commit

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,19 @@
+import { validate } from "../src/validateCpf";
+
+test.each([
+    "01234567890",
+    "71428793860",
+    "87748248800"
+])("Deve testar um CPF válido: %s", function (cpf: any) {
+    expect(validate(cpf)).toBe(true);
+})
+
+test.each([
+    undefined,
+    null,
+    "11111111111",
+    "0123456789010",
+    "0123456"
+])("Deve testar um CPF inválido: %s", function (cpf: any) {
+    expect(validate(cpf)).toBe(false);
+})
